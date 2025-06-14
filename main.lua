7aa64fc72a5a3187bbce548d483fe5647e9c016b
CosmicCynth <theshinyseal@gmail.com>
2025-06-14 16:00:15 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,22 +1,30 @@
 projectiles = require("projectiles")
+playeranimations = require("playeranimations")
+anim8 = require("libaries.anim8")
 player = require("player")
 
 
 function love.load()
+    --FPS
+    FPS = tostring(love.timer.getFPS())
     --Image rendering
     love.graphics.setDefaultFilter("nearest","nearest")
 
     --Modules
     player.load()
     projectiles.load()
+    playeranimations.load()
 end
 
 function love.update(dt)
     player.update(dt)
     projectiles.update(dt)
+
+    FPS = tostring(love.timer.getFPS())
 end
 
 function love.draw()
+    love.graphics.print(FPS)
     projectiles.draw()
     player.draw()
 end
